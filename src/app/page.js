26e5302a0b436b3d019cdc84693cc2e459c2873e
B0295868 <shaurya.pratapsingh@airtel.com>
2024-04-19 00:58:31 +0530

    api deploy and ui enhancment

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,12 @@ import About from '@/pages/about';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./globals.css";
+import Prompt from '@/pages/prompt';
 
 export default function Home() {
   return (
     <>
-      <HomePage />
+      <Prompt />
       <ToastContainer />
     </>
   );
--- src/pages/prompt.js
+++ src/pages/prompt.js
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from "react";
+"use client"; // This is a client component 👈🏽
+
+
+import React, { useState } from "react";
+import "../app/prompt.css";
+import logo from "../../public/Vector.png"
+import chatIcon from "../../public/chat.png";
+import Image from "next/image";
 
 const Prompt = () => {
   const [prompt, setPrompt] = useState("");
-  const [selectedModel, setSelectedModel] = useState();
+  const [selectedModel, setSelectedModel] = useState("MISTRALAI/MISTRAL-7B-INSTRUCT-V0.1");
+  const [tokenUsage, setTokenUsage] = useState(0);
+  const [outputLength, setOutputLength] = useState(300);
+  const [responseTime, setResponseTime] = useState(0);
   const [models, setModels] = useState([
     "MISTRALAI/MISTRAL-7B-INSTRUCT-V0.1",
     "MISTRALAI/MISTRAL-7B-INSTRUCT-V0.2",
@@ -11,25 +21,10 @@ const Prompt = () => {
     "TOGETHERCOMPUTER/LLAMA-2-7B-32K-INSTRUCT",
     "TOGETHERCOMPUTER/REDPAJAMA-INCITE-7B-CHAT",
     "TOGETHERCOMPUTER/REDPAJAMA-INCITE-CHAT-3B-V1",
-  ]); // Array to hold available models
-  const [response, setResponse] = useState(null); // State for API response
+  ]);
+  const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  // Fetch available fine-tuning models on component mount
-  //   useEffect(() => {
-  //     const fetchModels = async () => {
-  //       try {
-  //         const response = await fetch("http://localhost:3000/inference", {
-  //           method: "POST",
-  //         }); // Replace with your API endpoint
-  //         const data = await response.json();
-  //         setModels(data.models); // Update state with available models
-  //       } catch (error) {
-  //         console.error("Error fetching models:", error);
-  //       }
-  //     };
-  //     fetchModels();
-  //   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -38,17 +33,14 @@ const Prompt = () => {
       alert("Please enter a prompt and select a fine-tuning model.");
       return;
     }
-
+    const startTime = Date.now();
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:3000/inference", {
+      console.log(selectedModel)
+      const response = await fetch("https://fine-tuning-backend-t76cvz6sta-el.a.run.app/inference", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt, model: selectedModel }),
-        // body: JSON.stringify({
-        //   prompt: "Suggest some fun winter family activities",
-        //   model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-        // }),
       });
 
       if (!response.ok) {
@@ -56,8 +48,12 @@ const Prompt = () => {
       }
 
       const data = await response.json();
-      //   console.log(data.result.output.choices[0].text);
+      console.log(data.result.output.choices[0].text)
       setResponse(data.result.output.choices[0].text);
+      setTokenUsage(data.result.usage.total_tokens);
+      const endTime = Date.now(); // Capture end time
+      const durationInSeconds = (endTime - startTime) / 1000;
+      setResponseTime(durationInSeconds.toFixed(2));
     } catch (error) {
       console.error("Error calling fine-tuning API:", error);
       setResponse("An error occurred. Please try again later.");
@@ -65,41 +61,101 @@ const Prompt = () => {
     setLoading(false);
   };
 
+  const handleSliderChange = (event) => {
+    setOutputLength(event.target.value);
+  };
+
+  // Function to truncate the response
+  const truncateResponse = (response, maxLength) => {
+    return response && response.length > maxLength
+      ? response.substring(0, maxLength) + "..."
+      : response;
+  };
+
   return (
-    <div>
-      <h1>Fine-Tuning Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="prompt">Prompt:</label>
-        <textarea
-          id="prompt"
-          value={prompt}
-          onChange={(event) => setPrompt(event.target.value)}
-          rows="5"
-          placeholder="Enter your prompt here"
-        />
-
-        <label htmlFor="model">Fine-Tuning Model:</label>
-        <select
-          id="model"
-          value={selectedModel}
-          onChange={(event) => setSelectedModel(event.target.value)}
-        >
-          <option value="">Select a Model</option>
-          {models.map((model) => (
-            <option key={model} value={model}>
-              {model}
-            </option>
-          ))}
-        </select>
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <div className="response-container">
-        <h2>Response</h2>
-        <p>{loading ? "Loading..." : response}</p>
+    <>
+      <div className="logo-container">
+        <Image src={logo} alt="Logo" width={50} height={50} />
+        <span style={{ color: "#ccc" }}>Create Protocol</span>
       </div>
-    </div>
+      <div className="main-container">
+        <div className="left-container">
+          <div className="header-container">
+            <Image src={chatIcon} alt="Chat Icon" width={24} height={24} />
+
+            <h1 style={{ fontSize: "14px" }}>CHAT</h1>
+            {selectedModel && (
+              <div className="model-display">
+                {selectedModel}
+              </div>
+            )}
+          </div>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              id="prompt"
+              value={prompt}
+              onChange={(event) => setPrompt(event.target.value)}
+              rows="5"
+              placeholder="Enter text here"
+            />
+            <button
+              type="submit"
+              style={{
+                background: loading ? "grey" : "black",
+                color: "white"
+              }}
+              disabled={loading}
+            >
+              Submit
+            </button>
+            <div className="response-container">
+            <p>{loading ? "Loading..." : truncateResponse(response, outputLength)}</p>
+              {!loading && tokenUsage > 0 && (
+                <p className="token-usage">
+                  {tokenUsage} TOKENS | {(tokenUsage / responseTime).toFixed(2)} TOKENS/S
+                </p>
+              )}
+            </div>
+          </form>
+        </div>
+        <div className="right-container">
+          <label htmlFor="model">Fine-Tuning Model:</label>
+          <select
+            id="model"
+            value={selectedModel}
+            onChange={(event) => setSelectedModel(event.target.value)}
+          >
+            <optgroup label="Mistral Models">
+              {models.filter(model => model.includes("MISTRALAI")).map((model) => (
+                <option key={model} value={model}>
+                  {model}
+                </option>
+              ))}
+            </optgroup>
+            <optgroup label="Together Computer Models">
+              {models.filter(model => model.includes("TOGETHERCOMPUTER")).map((model) => (
+                <option key={model} value={model}>
+                  {model}
+                </option>
+              ))}
+            </optgroup>
+          </select>
+          <label htmlFor="output-length">Output Length:</label>
+          <input
+            type="range"
+            id="output-length"
+            min="0"
+            max="1000" // Max characters you want to allow
+            value={outputLength}
+            onChange={handleSliderChange}
+        
+          />
+          <p>Characters: {outputLength}</p>
+
+        </div>
+      </div>
+    </>
+
   );
 };
 
